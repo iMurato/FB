af951c2399dbd1403ff3c5d35ae91aa682d3ffad
iMurato <i.murato@mail.ru>
2020-08-02 17:57:43 +0400

    09

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Animator Connect;
+    
+    private bool OnPauseSkip;
+    
+    private void Awake()
+    {
+        FirebaseController.MyData.Add("Sign", "0");
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnConnectClick()
+    {
+        FirebaseController.Connect();
+        
+        Connect.Play("Ui-Push-Disable");
+    }
+
+    #if UNITY_EDITOR
+
+        private void OnApplicationQuit()
+        {
+            FirebaseController.Disconnect();
+        }
+
+    #else
+    
+        private void OnApplicationPause(bool OnPause)
+        {
+            if (OnPause && OnPauseSkip)
+            {
+                FirebaseController.Disconnect();
+            }
+            else
+            {
+                OnPauseSkip = true;
+            }
+        }
+        
+    #endif
+}
